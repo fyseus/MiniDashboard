22092fff7c3dd813c7318326bdb0b2d8ff875c4c
fyseus <fyseus@163.com>
2022-04-04 19:28:15 +0800

    - tomcat added

---
+++ src/main/java/com/jdk/servlet/TestServlet.java
@@ -0,0 +1,30 @@
+package com.jdk.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Yang fyseus
+ * @created 2022/4/4
+ * @project MiniDashboard
+ */
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.write("My Servlet");
+        pw.flush();
+        pw.close();
+    }
+}
